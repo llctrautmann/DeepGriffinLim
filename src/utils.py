82c9e8572d8a7a48e54a8d53ccc0d0d8b43e5159
Luca Trautmann <guy_montag@posteo.net>
2023-08-23 10:37:01 +0100

    Add seed everything function

--- src/utils.py
+++ src/utils.py
@@ -95,6 +95,16 @@ import torchvision
 #     except Exception as e:
 #         print(f"An error occurred: {e}")
 
+def seed_everything(seed=42):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
 
 class HealthCheckDashboard:
 
