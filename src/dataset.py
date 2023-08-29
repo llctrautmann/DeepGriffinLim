b9e2b6cd272fa68397a0ed0fc69bf865f00a8465
Luca Trautmann <guy_montag@posteo.net>
2023-08-29 20:39:03 +0200

    Change functionality of the data loader
    The data loader is now using a uniform sampling rate of 48khz and is
    spliting the files according to the seconds in the hyperparameter
    file

--- src/dataset.py
+++ src/dataset.py
@@ -4,7 +4,6 @@ from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 import torchaudio
 import random
-
 from hyperparameter import hp
 
 class AvianNatureSounds(Dataset):
@@ -23,7 +22,6 @@ class AvianNatureSounds(Dataset):
                 downsample=True,
                 mel_spectrogram = None,
                 verbose=False,
-                fixed_limit=False
                 ):
         
         self.column = key
@@ -37,8 +35,6 @@ class AvianNatureSounds(Dataset):
         self.hop_length = hop_length
         self.downsample = downsample
         self.sampling_rate = sampling_rate
-        self.verbose = verbose
-        self.fixed_limit = fixed_limit
         self.signal_length = None
         self.griffin_lim = torchaudio.transforms.GriffinLim(n_fft=self.n_fft, 
                                                             win_length=self.n_fft,
@@ -46,27 +42,12 @@ class AvianNatureSounds(Dataset):
                                                             power=2,
                                                             n_iter=5,
                                                             momentum=0.99)
-        self.return_signal_dims()
-
-    def return_signal_dims(self):
-        if self.verbose:
-            print(f'Returning signal dimensions: H = {self.n_fft // 2 + 1} W = {((self.sampling_rate * self.length)//self.hop_length) + 1}')
-        else:
-            pass
-
 
     def __len__(self):
         return len(self.annotation_file)
     
-    def _update_signal_length(self,signal_length):
-        self.signal_length = signal_length
-        
-
 
     def __getitem__(self, index):
-        ####################################################################################
-        # Get stft spectrograms ############################################################
-        ####################################################################################
         if self.mode == 'stft':
             audio_sample_path = os.path.join(self.root_dir,os.listdir(self.root_dir)[index])
             label = self.annotation_file.iloc[index][self.column]
@@ -78,63 +59,45 @@ class AvianNatureSounds(Dataset):
                 pass
 
             # Clip the signal to the desired length
-            signal = self.clip(signal, sr, self.length,fixed_limit=self.fixed_limit)
+            signal = self.clip(signal, sr, self.length)
             # print(f'{signal.shape} = clipped signal shape')
 
             stft = torch.stft(signal, n_fft=self.n_fft, hop_length=self.hop_length,win_length=self.n_fft, normalized=False, return_complex=True)
 
-
             # Add complex Gaussian noise to the complex tensor
             noise_real = torch.randn_like(stft.real)
             noise_imag = torch.randn_like(stft.imag)
-            noisy_sig = stft + (noise_real + 1j * noise_imag)
 
+            noise = torch.complex(noise_real, noise_imag)
 
+            # Compute signal power and noise power
+            signal_power = torch.mean(stft.abs().square())
+            noise_power = torch.mean(noise.abs().square())
+
+            random_snr = torch.rand(1).item() * 6 - 6     
+             # Compute the scaling factor for the noise
+            K = torch.sqrt(signal_power / (noise_power * 10**(random_snr / 10)))
+            
+            # Scale the noise and add it to the original signal
+            noisy_sig = stft + K * noise
+            
+            # noisy_sig = stft + (noise_real + 1j * noise_imag)
             magnitude = torch.abs(stft) # 25 Jul 2023 @ 12:21:38 ### CHANGED ###
 
-            return stft, noisy_sig,  magnitude , label
+            return stft, noisy_sig, magnitude, label
 
 
     @staticmethod
     @torch.no_grad()
-    def clip(audio_signal, sr, desired_length,fixed_limit=False):
+    def clip(audio_signal, sr, desired_length):
         sig_len = audio_signal.shape[1]
         length = int(sr * desired_length)
-
-        if fixed_limit:
-            sig = audio_signal[0][:262100]
-            return  sig.unsqueeze(0)
-
-        elif sig_len > length:
+        if sig_len > length:
             offset = random.randint(0, sig_len - length)
-            sig = audio_signal[:, offset:(offset+length)]
-
-            return sig
-        elif fixed_limit is None:
+            audio_signal = audio_signal[:, offset:(offset+length)]
             return audio_signal
-        
-    @staticmethod
-    @torch.no_grad()
-    def _resize_signal_length(signal, signal_length):
-        if signal.shape[-1] > signal_length:
-            signal = signal[...,:signal_length]
-            return signal
-        elif signal.shape[-1] < signal_length:
-            length_diff = signal_length - len(signal[-1])
-
-            prefix = torch.zeros((1,length_diff//2))
-            suffix = torch.zeros((1,length_diff//2))
-            signal = torch.cat([prefix,signal,suffix],dim=-1)
-
-            if len(signal[-1]) == signal_length:
-                return signal
-            else:
-                length_diff = signal_length - len(signal[-1])
-                signal = torch.cat([signal,torch.zeros((1,length_diff))],dim=-1)
-                return signal
         else:
-            return signal
-        
+            return audio_signal
     
     @staticmethod
     @torch.no_grad()
@@ -150,7 +113,7 @@ class AvianNatureSounds(Dataset):
         Returns:
         Tensor: Downsampled waveform.
         """
-        transform = torchaudio.transforms.Resample(orig_freq=orig_freq, new_freq=new_freq)
+        transform = torchaudio.transforms.Resample(orig_freq=orig_freq, new_freq=new_freq, lowpass_filter_width=128)
         return transform(waveform)
 
 
@@ -162,7 +125,4 @@ ds = AvianNatureSounds(annotation_file_path=hp.annotation_file_path,
                        sampling_rate=hp.sampling_rate,
                        n_fft=hp.n_fft,
                        hop_length=hp.hop_length,
-                       mel_spectrogram=hp.mel_spectrogram,
-                       verbose=hp.verbose,
-                       fixed_limit=False)
-
+                       mel_spectrogram=hp.mel_spectrogram)
