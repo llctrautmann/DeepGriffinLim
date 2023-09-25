afc1ce0b4e19dd1974d250db2a4aa4a7fb35df4c
Luca Trautmann <guy_montag@posteo.net>
2023-09-25 21:58:34 +0200

    Move GLA to increase efficiency

--- src/dataset.py
+++ src/dataset.py
@@ -97,11 +97,12 @@ class AvianNatureSounds(Dataset):
             random_noise = torch.complex(noise_real, noise_imag)
 
             # Perform Griffin-Lim reconstruction
-            gla_pretrain = torch.stft(self.griffin_lim(magnitude), n_fft=1024, hop_length=512, return_complex=True)
+            # gla_pretrain = torch.stft(self.griffin_lim(magnitude), n_fft=1024, hop_length=512, return_complex=True)
 
             if hp.data_mode == 'denoise':
                 return stft, noisy_sig, magnitude, label
             elif hp.data_mode == 'gla-pretrain':
+                gla_pretrain = torch.stft(self.griffin_lim(magnitude), n_fft=1024, hop_length=512, return_complex=True)
                 return stft, gla_pretrain , magnitude, label
             else:
                 return stft, random_noise, magnitude, label
